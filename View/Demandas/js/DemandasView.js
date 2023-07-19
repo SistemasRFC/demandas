6230ef877039b1d52fdbd55291df3584273a139b
Fernanda Lopes <33816156+FernandaLA@users.noreply.github.com>
2023-07-19 10:18:04 -0300

    Update DemandasView.js

--- View/Demandas/js/DemandasView.js
+++ View/Demandas/js/DemandasView.js
@@ -77,6 +77,7 @@ function editarDemanda(indice) {
     $("#descricaoEdit").removeClass("show");
     $("#accordionDscEdit").html("");
     $("#updateDemandaTitle").html('Editar Demanda');
+    $("#descricao").removeClass("show");
     $("#accordionEdit").show();
     $("#updateDemanda").modal('show');
 }
